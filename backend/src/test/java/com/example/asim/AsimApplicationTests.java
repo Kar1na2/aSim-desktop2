60e3ba1c908817dfa6aaf608ec06fd7b2964428e
Joon Park <jspark6@dons.usfca.edu>
2026-04-06 19:42:43 -0700

    setting up backend

---
+++ backend/src/test/java/com/example/asim/AsimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
